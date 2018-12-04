b973d72a9b9e7089a838eae6d69f25428bc77403
Alex Ferrer <ferrerale@media-saturn.com>
2018-12-04 17:09:34 +0100

    RabbitMq authentication

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,16 @@
 FROM rabbitmq:3-management
 
 RUN rabbitmq-plugins enable --offline rabbitmq_web_mqtt
-RUN rabbitmq-plugins enable rabbitmq_auth_backend_http
+RUN rabbitmq-plugins enable --offline rabbitmq_auth_backend_http
+
+# Define environment variables.
+ENV RABBITMQ_USER admin
+ENV RABBITMQ_PASSWORD example
+
+ADD ./docker-config/rabbitmq/init.sh /init.sh
+RUN chmod +x /init.sh
 
 EXPOSE 15671 15672 15675
+
+# Inititlization
+CMD ["/init.sh"]
--- mqtt-php-auth-server/src/routes.php
+++ mqtt-php-auth-server/src/routes.php
@@ -12,6 +12,19 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
-$app->post('/[{name}]', function (Request $request, Response $response, array $args) {
-    
+// RabbitMQ backend authentication
+$app->post('/auth/user', function (Request $request, Response $response, array $args) {
+    return $response->withStatus(200)->write('allow');
+});
+
+$app->post('/auth/vhost', function (Request $request, Response $response, array $args) {
+    return $response->withStatus(200)->write('allow');
+});
+
+$app->post('/auth/resource', function (Request $request, Response $response, array $args) {
+    return $response->withStatus(200)->write('allow');
+});
+
+$app->post('/auth/topic', function (Request $request, Response $response, array $args) {
+    return $response->withStatus(200)->write('allow');
 });
